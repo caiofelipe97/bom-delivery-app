07c8aeaa36e007c4f9fb270d957edb67c7e2923e
Caio Melo <caiofelipee17@gmail.com>
2020-10-28 15:59:41 -0300

    Config navigation

---
+++ src/routes/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import CellphoneConfirmation from '../pages/CellphoneConfirmation';
+import EmailConfirmation from '../pages/EmailConfirmation';
+
+const Auth = createStackNavigator();
+
+const AuthRoutes: React.FC = () => (
+  <Auth.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: '#F6F2F8' },
+    }}
+  >
+    <Auth.Screen name="SignIn" component={SignIn} />
+    <Auth.Screen name="SignUp" component={SignUp} />
+    <Auth.Screen
+      name="CellphoneConfirmation"
+      component={CellphoneConfirmation}
+    />
+    <Auth.Screen name="EmailConfirmation" component={EmailConfirmation} />
+  </Auth.Navigator>
+);
+
+export default AuthRoutes;
