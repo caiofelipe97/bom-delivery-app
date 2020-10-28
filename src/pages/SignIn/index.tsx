3e79175d341e4ca87f12bea60f816e064e580839
Caio Melo <caiofelipee17@gmail.com>
2020-10-28 19:35:29 -0300

    Add SignIn Page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,60 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, StatusBar } from 'react-native';
 
-import { Container } from './styles';
+import {
+  Container,
+  Background,
+  Content,
+  FacebookButton,
+  FacebookButtonText,
+  TermsButton,
+  TermsText,
+} from './styles';
 
 import logoImg from '../../assets/logo.png';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-    </Container>
+    <>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="#78308C"
+      />
+      <Container>
+        <Background>
+          <Image source={logoImg} />
+        </Background>
+        <Content>
+          <FacebookButton
+            onPress={() => {
+              console.log('Login Facebbok');
+            }}
+          >
+            <FacebookButtonText>ENTRAR COM FACEBOOK</FacebookButtonText>
+          </FacebookButton>
+          <Button
+            style={{ marginTop: 32 }}
+            onPress={() => {
+              console.log('Login Email');
+            }}
+          >
+            ENTRAR COM E-MAIL
+          </Button>
+        </Content>
+        <TermsButton
+          onPress={() => {
+            console.log('Ir para termos');
+          }}
+        >
+          <TermsText>
+            Ao entrar, você concorda com nossos Termos de Uso e Política de
+            Dados.
+          </TermsText>
+        </TermsButton>
+      </Container>
+    </>
   );
 };
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,3 +1,63 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Background = styled.View`
+  width: 100%;
+  height: 60%;
+  align-items: center;
+  justify-content: center;
+  background: #78308c;
+  box-shadow: 0px 4px 30px rgba(0, 0, 0, 0.06);
+  border-bottom-right-radius: 30px;
+  border-bottom-left-radius: 30px;
+`;
+
+export const Content = styled.View`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
+`;
+
+export const FacebookButton = styled(RectButton)`
+  width: 100%;
+  height: 60px;
+  background: #4267b2;
+  border-radius: 10px;
+  margin-top: 40px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FacebookButtonText = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  color: #f6f6f9;
+  font-size: 18px;
+`;
+
+export const TermsButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  border-color: #232129;
+  padding: 16px 0 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const TermsText = styled.Text`
+  color: #888888;
+  font-family: 'RobotoSlab-Regular';
+  font-size: 17px;
+  text-align: center;
+`;
